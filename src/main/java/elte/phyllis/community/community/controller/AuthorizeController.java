60df889f3843bc13a69cc12c0bffd5a3f31f1632
phyllis <1741922768@qq.com>
2020-04-26 10:49:31 +0200

    implement insert user information to H2 database

--- src/main/java/elte/phyllis/community/community/controller/AuthorizeController.java
+++ src/main/java/elte/phyllis/community/community/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package elte.phyllis.community.community.controller;
 import com.sun.javaws.jnl.IconDesc;
 import elte.phyllis.community.community.dto.AccessTokenDTO;
 import elte.phyllis.community.community.dto.GithubUser;
+import elte.phyllis.community.community.mapper.UserMapper;
+import elte.phyllis.community.community.model.User;
 import elte.phyllis.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
-
     @Autowired
     private GithubProvider githubProvider;
     @Value("${github.client.id}")
@@ -22,11 +26,14 @@ public class AuthorizeController {
     @Value("${github.client.redirect_uri}")
     private String clientRedirect_uri;
 
+    @Autowired(required=false)
+    private UserMapper userMapper;
 
-    //caback重定向后返回到index页面
+    //callback重定向后返回到index页面
     @GetMapping("/callback")//这里面传递的参数是要从url里面获取的，第一个是code, 还有一个是state
     public String callback(@RequestParam (name="code") String code,
-                           @RequestParam (name="state") String state){
+                           @RequestParam (name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,9 +41,23 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(clientRedirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+//        System.out.println(user.getName());
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());//用UUID的随机id
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写session,session 是在HttpServletRequest request里面拿到的,把user对象放到session里面
+            request.getSession().setAttribute("user",githubUser);
+            //登录成功后重定向到默认的index页面，这里的参数只能是mapping的定义的路由，不能直接写index
+            return "redirect:/";
+        }else{
+        //登录失败
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/elte/phyllis/community/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package elte.phyllis.community.community.mapper;
+import elte.phyllis.community.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
