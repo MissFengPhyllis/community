fbf2436688242f16e0c35c1be6b08b04fd5c8a03
phyllis <1741922768@qq.com>
2020-04-30 14:00:48 +0200

    add profile questions page and the pagination in the secondary url

--- src/main/java/elte/phyllis/community/community/mapper/QuestionMapper.java
+++ src/main/java/elte/phyllis/community/community/mapper/QuestionMapper.java
@@ -20,4 +20,10 @@ public interface QuestionMapper {
 
     @Select("select count(1) from question")
     Integer count();
+
+    @Select("select count(1) from question where creator=#{userId}")
+    Integer countByUserId(@Param(value = "userId") Integer userId);
+
+    @Select("select * from question where creator=#{userId} order by gmt_modified desc limit #{offset},#{size}" )
+    List<Question> listByUserId(@Param(value="userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
 }
--- src/main/java/elte/phyllis/community/community/services/QuestionServices.java
+++ src/main/java/elte/phyllis/community/community/services/QuestionServices.java
@@ -49,9 +49,35 @@ public class QuestionServices {
             questionDTOList.add(questionDTO);
         }
         paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+    }
 
+    public PaginationDTO listByUserId(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.countByUserId(userId);
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page<1){
+            page = 1;
+        }else if(page>paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page-1)
+        Integer offset  =size * (page-1);
+        List<Question> questions= questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+            //这里要找到user的头像，肯定是传递的是question里面的creator的值啊，肯定不是getID，那个question的ID，跟USERID 没关系
+            User user = userMapper.findById(question.getCreator());
+            //把question里面的对象都放入到DTO里面去
+            QuestionDTO questionDTO = new QuestionDTO();
 
-
+            //这里可以用传统的方法，一个一个的去设置questionDTO.setId(question.getId());
+            //这里使用spring的一个复制方法
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
     ///目的在于这里面不仅可以使用questionmapper，还能用usermapper，起到一个组装的作用
